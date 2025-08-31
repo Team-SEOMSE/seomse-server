f204a4d95aa617a6d53ea4631ef58359ba44199f
gioJioh <sjgio0515@naver.com>
2025-08-31 19:19:18 +0900

    refactor: 수정사항 반영, @ResponseStatus

--- src/main/java/com/seomse/interaction/appointment/controller/AppointmentController.java
+++ src/main/java/com/seomse/interaction/appointment/controller/AppointmentController.java
@@ -2,9 +2,11 @@ package com.seomse.interaction.appointment.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,6 +25,7 @@ public class AppointmentController {
 	private final AppointmentService appointmentService;
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public ApiResponse<UUID> createAppointment(
 		@Valid @RequestPart AppointmentCreateRequest request,
 		@RequestPart(required = false) MultipartFile requirementsImage) {
