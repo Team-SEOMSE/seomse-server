8bad58f31f72710db7f6f257f17a735e50bbb7b9
gioJioh <sjgio0515@naver.com>
2025-08-31 19:48:19 +0900

    refactor: 수정사항 반영, @mockito, givenwhenthen

--- src/test/java/com/seomse/IntegrationTestSupport.java
+++ src/test/java/com/seomse/IntegrationTestSupport.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import com.seomse.security.feign.kakao.KakaoApiFeignCall;
 import com.seomse.security.service.SecurityService;
 
+import software.amazon.awssdk.services.s3.S3Client;
+
 @ActiveProfiles("local")
 @SpringBootTest
 public abstract class IntegrationTestSupport {
@@ -16,6 +18,9 @@ public abstract class IntegrationTestSupport {
 
 	@MockitoBean
 	protected KakaoApiFeignCall kakaoApiFeignCall;
+
+	@MockitoBean
+	protected S3Client s3Client;
 
 }
 
