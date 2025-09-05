88ec6fa66e383f37fb7e11976998011b650bb365
HOYA <102270909+mingeonho1@users.noreply.github.com>
2025-09-05 23:51:44 +0900

    fix: BINARY로 변환 수정 (#37)

--- src/main/java/com/seomse/shop/entity/DesignerShopEntity.java
+++ src/main/java/com/seomse/shop/entity/DesignerShopEntity.java
@@ -2,7 +2,9 @@ package com.seomse.shop.entity;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import com.seomse.common.entity.BaseTimeEntity;
 import com.seomse.user.designer.entity.DesignerEntity;
@@ -31,6 +33,7 @@ public class DesignerShopEntity extends BaseTimeEntity {
 
 	@Id
 	@UuidGenerator
+	@JdbcTypeCode(SqlTypes.BINARY)
 	@Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
 	private UUID id;
 
